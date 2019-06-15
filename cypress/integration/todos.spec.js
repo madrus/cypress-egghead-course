cdeb1ffe4a7f4f0688a1a857c9e3e3c358269e45
Andre Roussakoff <madrus@gmail.com>
2019-06-15 09:42:22 +0200

    small formatting

--- cypress/integration/todos.spec.js
+++ cypress/integration/todos.spec.js
@@ -3,12 +3,14 @@
 describe('Todo Application', () => {
   it('loads the home page', () => {
     cy.visit('/')
+
     cy
       .get('.todo-list li:nth-child(1)')
       .should('have.text', 'Hello world')
       .should('not.have.class', 'completed')
       .find('.toggle')
       .should('not.be.checked')
+
     cy
       .get('.todo-list li:nth-child(2)')
       .should('have.text', 'Goodnight moon')
